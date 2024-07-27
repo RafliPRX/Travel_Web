99c2972593f194a659ac2ac51527652251fae045
RafliPRX <raflip72@gmail.com>
2024-07-27 23:46:32 +0800

    secon commit

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("token");
   if (token == null) {
-    return <Navigate to={"/login"} />;
+    return <Navigate to={"/"} />;
   }
 
   return <>{children || <Outlet />}</>;
